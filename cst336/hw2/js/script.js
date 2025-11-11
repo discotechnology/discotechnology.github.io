0dce013891e45de020bb9e3cf7e713f47a047acd
discotechnology <drosenfeld@csumb.edu>
2025-11-10 19:01:59 -0800

    Added styles

--- cst336/hw2/js/script.js
+++ cst336/hw2/js/script.js
@@ -1,5 +1,8 @@
-let correctPoints = 20;
+let correctPoints = 10;
 var score = 0;
+var attempts = localStorage.getItem("total_attempts");
+
+displayQ4Choices();
 
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
@@ -10,9 +13,11 @@ function gradeQuiz(){
         return;
     }
 
+    // Answers
     score = 0;
     let q1Response = document.querySelector("#q1").value.toLowerCase();
     let q2Response = document.querySelector("#q2").value;
+    let q4Response = document.querySelector('input[name="q4"]:checked').value;
 
     // Question 1
     if (q1Response == "sacramento") {
@@ -42,8 +47,17 @@ function gradeQuiz(){
         wrongAnswer(3);
     }
 
+    // Question 4
+    if (q4Response == "Rhode Island") {
+        rightAnswer(4);
+    }
+    else {
+        wrongAnswer(4);
+    }
 
     document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
+    document.querySelector("#totalAttempts").innerHTML = `Total Attempts: ${++attempts}`;
+    localStorage.setItem("total_attempts", attempts);
 
 }
 
@@ -51,13 +65,26 @@ function isFormValid() {
     let isValid = true;
     if (document.querySelector("#q1").value == "") {
         isValid = false;
-        document.querySelector("#validationFeedback").textContent = "Question 1 was not answered. ";
+        document.querySelector("#validationFeedback").textContent = "Answer Q1. ";
     }
     if (document.querySelector("#q2").value == "") {
         isValid = false;
-        document.querySelector("#validationFeedback").textContent += "Question 2 was not answered. ";
+        document.querySelector("#validationFeedback").textContent += "Answer Q2. ";
+    }
+    if (document.querySelector('input[name="q4"]:checked') == null) {
+        isValid = false;
+        document.querySelector("#validationFeedback").textContent += "Answer Q3. ";
     }
     return isValid;
+}
+
+function displayQ4Choices() {
+    let choices = ["Maine", "Delaware", "Rhode Island", "Maryland"];
+    choices = _.shuffle(choices);
+    for (let i = 0; i < choices.length; i++) {
+        document.querySelector("#q4Choices").innerHTML +=
+            `<input type="radio" name="q4" value="${choices[i]}"> <label for="${choices[i]}"> ${choices[i]}</label> `;
+    }
 }
 
 function rightAnswer(index) {
