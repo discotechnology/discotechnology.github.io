8b29c1ba5f6d4ba4d4bcb6fbfe48c851791dba1f
discotechnology <drosenfeld@csumb.edu>
2025-11-15 16:06:47 -0800

    Finished tutorial

--- cst336/lab3/js/script.js
+++ cst336/lab3/js/script.js
@@ -2,6 +2,9 @@
 
 document.querySelector("#zip").addEventListener("change",displayCity);
 document.querySelector("#state").addEventListener("change",displayCounties);
+document.querySelector("#username").addEventListener("change",checkUsername);
+document.querySelector("#signupForm").addEventListener("submit", function(event) {validateForm(event);});
+
 
 //Functions
 
@@ -31,4 +34,55 @@ async function displayCounties() {
     for(i of data) {
         document.querySelector("#county").innerHTML += `<option> ${i.county} </option>`
     }
+}
+
+async function checkUsername() {
+    let username = document.querySelector("#username").value;
+    let url = `https://csumb.space/api/usernamesAPI.php?username=${username}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let validation = document.querySelector("#usernameValidation");
+
+    if(data.available) {
+        validation.textContent = " Username is available!";
+        validation.style.color = "green";
+    }
+    else {
+        validation.textContent = " Username is taken - choose another username.";
+        validation.style.color = "red";
+    }
+}
+
+function validateForm(e) {
+    let isValid = true;
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#pwd").value;
+    let confirm = document.querySelector("#pwdConfirm").value;
+
+    document.querySelector("#passwordError").textContent = "";
+
+    if(username == "") {
+        document.querySelector("#usernameValidation").textContent = "Username required.";
+        document.querySelector("#usernameValidation").style.color = "red";
+        isValid = false;
+    }
+
+    if(password.length < 6) {
+        document.querySelector("#passwordError").textContent += "Password must be at least 6 characters. "
+        document.querySelector("#passwordError").style.color = "red";
+        isValid = false;
+    }
+
+    if(password != confirm) {
+        document.querySelector("#passwordError").textContent += "Passwords entered do not match."
+        document.querySelector("#passwordError").style.color = "red";
+        isValid = false;
+    }
+
+    if(!isValid) {
+        e.preventDefault();
+    }
+    
 }
